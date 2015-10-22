28efa68ad42b6cf73996ac9a25235c30b13177cf
Diogo Monteiro <diogo.p.monteiro@ist.utl.pt>
2015-10-22 18:12:17 +0100

    mad skillz tweaks

--- core/management/wm.py
+++ core/management/wm.py
@@ -39,12 +39,23 @@ class Command(object):
         method(image_file)
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def execute_command():
     try:
         import sys
         Command.execute(*sys.argv[1:])
     except WmError as w:
-        print "WmError: " + w.message
+        print bcolors.FAIL + bcolors.BOLD + "WmError: " + bcolors.ENDC + bcolors.BOLD + w.message + bcolors.ENDC
 
 
 
