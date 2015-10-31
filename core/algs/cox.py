4018e83dee69dabaa51ccfc56e9f5365ab4d86c9
João Santos <jnuno1994@gmail.com>
2015-10-31 01:43:28 +0000

    Improvements

--- core/algs/cox.py
+++ core/algs/cox.py
@@ -1,4 +1,3 @@
-import numpy
 from core.algs.abstract import Algorithm
 from PIL import Image
 from core.algs.utils import TwoDimensionalDCT
@@ -6,6 +5,9 @@ import numpy
 
 
 class Cox(Algorithm):
+
+    mu = 127
+    sigma = 255
 
     def compare(self, original_watermark, extracted_watermark):
         pass
@@ -18,24 +20,29 @@ class Cox(Algorithm):
         # Compute DCT
         fDct = TwoDimensionalDCT.forward(image)
         # Sort DCT
-        sortedDCTindexes = numpy.argsort(fDct)[::-1]
+        sizeDCT = int(fDct.__len__()*0.1)
+        sortedDCTindexes = fDct.ravel().argsort()[-sizeDCT:]
+        sortedDCTindexes = (numpy.unravel_index(indx, fDct.shape) for indx in sortedDCTindexes)
+        # sortedUnraveled = []
+        # for i in sortedDCTindexes:
+        #     sortedUnraveled.append((sortedDCTindexes[i], i))
+        sortedUnraveled = [(fDct[indx], indx) for indx in sortedDCTindexes]
         # Get size of watermark
         if(watermark!=None):
-            watermarkImage= Image.open(watermark)
+            watermarkImage = Image.open(watermark)
             wmAux = watermarkImage.size()
             nbits = wmAux[0]*wmAux[1]
             for i in range(nbits):
                 # I am so lost...
-                fDct[i][sortedDCTindexes[i]] = fDct[i][sortedDCTindexes[i]] * \
-                                               (TwoDimensionalDCT().forward(watermarkImage)[sortedDCTindexes[i]][i])
+                fDct[sortedUnraveled[i][1]] = fDct[sortedUnraveled[i][1]] *(TwoDimensionalDCT().forward(watermarkImage)[sortedDCTindexes[i]][i])
         else:
             imgSize = int(fDct.__len__())
-            nbits = numpy.random.normal(127, 255, imgSize)
+            nbits = numpy.random.normal(self.mu, self.sigma, sizeDCT)
         # Construct the Watermark
             for i in range(len(nbits)):
-                fDct[i][sortedDCTindexes[i]] = fDct[i][sortedDCTindexes[i]] * nbits[i]
+                fDct[sortedUnraveled[i][1]] = fDct[sortedUnraveled[i][1]] * nbits[i]
         inverse = TwoDimensionalDCT.inverse(fDct)
-        return image
+        return inverse
 
     def extract_specific(self, image, watermark):
         pass
