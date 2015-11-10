da4f130928910f1a2282834aeabf9d67ec95ccbc
Diogo Monteiro <diogo.p.monteiro@ist.utl.pt>
2015-11-10 03:41:38 +0000

    yeah.... science

--- core/algs/dwt3.py
+++ core/algs/dwt3.py
@@ -7,8 +7,8 @@ from core.algs.utils import TwoDimensionalDCT, Metrics
 
 class DWT(Algorithm):
 
-    WAVELET = 'db1'
-    alpha = 0.01
+    WAVELET = 'haar'
+    alpha = 0.1
 
     def split_image(self, image):
         return image[:, :, 2], image[:, :, 1], image[:, :, 0]
@@ -24,10 +24,10 @@ class DWT(Algorithm):
         return result
 
     def rgb_to_dwt(self, r, g, b):
-        return dwt2(r, self.WAVELET), dwt2(g, self.WAVELET), dwt2(b, self.WAVELET)
+        return dwt2(r, self.WAVELET, mode='sym'), dwt2(g, self.WAVELET, mode='sym'), dwt2(b, self.WAVELET, mode='sym')
 
     def dwt_to_rgb(self, cr, cg, cb):
-        return idwt2(cr, self.WAVELET), idwt2(cg, self.WAVELET), idwt2(cb, self.WAVELET)
+        return idwt2(cr, self.WAVELET, mode='sym'), idwt2(cg, self.WAVELET, mode='sym'), idwt2(cb, self.WAVELET, mode='sym')
 
     def rgb_to_dct(self, r, g, b):
         return TwoDimensionalDCT.forward(r), TwoDimensionalDCT.forward(g), TwoDimensionalDCT.forward(b)
@@ -42,20 +42,24 @@ class DWT(Algorithm):
         img = self.split_image(wm)
         dcts_wm = self.rgb_to_dct(*img)
 
-        xr, xg, xb = self.dct_to_rgb(*dcts_wm)
-
         dwts_i = self.rgb_to_dwt(ir, ig, ib)
 
         for color in range(3):
             dct = dcts_wm[color]
-            hh = dwts_i[color][1][2]
+            hh = TwoDimensionalDCT.forward(dwts_i[color][1][2])
             for j in range(len(dct)):
                 for k in range(len(dct[0])):
                     hh[j][k] += dct[j][k] * self.alpha
 
+            ihh = TwoDimensionalDCT.inverse(hh)
+            hh = dwts_i[color][1][2]
+            for j in range(len(dct)):
+                for k in range(len(dct[0])):
+                    hh[j][k] = ihh[j][k]
+
         rgb = self.dwt_to_rgb(*dwts_i)
+
         img = self.join_image(*rgb)
-        print img
         return img
 
     def extract_specific(self, image, watermark):
@@ -72,10 +76,17 @@ class DWT(Algorithm):
         image_dwt = list(self.rgb_to_dwt(*image_rgb))
         wm_dwt = list(self.rgb_to_dwt(*wm_rgb))
 
+
         for color in range(3):
+            dct_image_dwt = TwoDimensionalDCT.forward(image_dwt[color][1][2])
+            dct_wm_dwt = TwoDimensionalDCT.forward(wm_dwt[color][1][2])
             for i in range(len(image_dwt[RED][1][2])):
                 for j in range(len(image_dwt[RED][1][2][0])):
-                    image_dwt[color][1][2][i][j] = (wm_dwt[color][1][2][i][j] - image_dwt[color][1][2][i][j])/self.alpha
+                    dct_image_dwt[i][j] = (dct_wm_dwt[i][j] - dct_image_dwt[i][j])/self.alpha
+
+            for i in range(len(image_dwt[RED][1][2])):
+                for j in range(len(image_dwt[RED][1][2][0])):
+                    image_dwt[color][1][2][i][j] = dct_image_dwt[i][j]
 
         return self.join_image(*self.dct_to_rgb(image_dwt[RED][1][2], image_dwt[GREEN][1][2], image_dwt[BLUE][1][2])), 0
 
