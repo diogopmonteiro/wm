2d99d8b87a3e195cb5db3a1dd170eff55be1c1ad
Pedro Reganha <pedro.reganha@gmail.com>
2015-11-30 23:52:46 +0000

    skeleton

---
+++ core/algs/recover.py
@@ -0,0 +1,10 @@
+from abstract import Algorithm
+
+
+class Recover(Algorithm):
+
+    def embed_specific(self, image, image_file, watermark=None):
+        pass
+
+    def extract_specific(self, image, watermark):
+        pass
