e7152056683aaa31229b1dc8c99e8184d0b619f8
Nuno Santos <jnuno1994@gmail.com>
2015-12-04 06:59:56 +0000

    asidohoa

--- core/algs/recover.py
+++ core/algs/recover.py
@@ -25,6 +25,9 @@ class Recover(Algorithm):
         # Randomly pick a prime number k E [1, N - 1]
         k = self.get_k(self.N)
 
+        with open(Algorithm().get_image_output_file('{0}_k'.strip('.jpg').format(image_file)), 'w+') as fd:
+            fd.write(str(k))
+
         # For each block number X, apply Eq. (3) to obtain X' the number of its mapping block.
         # Record all pairs of X and X' to form the block mapping sequence
         cor = {}
@@ -211,3 +214,125 @@ class Recover(Algorithm):
 
     def extract_specific(self, image, watermark):
         pass
+
+    def get_bit_value(self, value, bit):
+        snum = "{0:b}".format(value)
+        if len(snum) <= bit:
+            return 0
+        else:
+            return int(snum[bit])
+
+
+    def tamper_detection_level_1(self, image, image_file):
+        erroneous = False
+
+        with open(Algorithm.get_image_output_file('{0}_k'.strip('.jpg').format())) as fd:
+            k = fd.read()
+
+        k = int(k)
+
+        r,g,b = self.split_image(image)
+
+        # Divide the image into non-overlapping blocks of 2X2 pixels
+        r_matrix, g_matrix, b_matrix = self.divide_in_blocks(r,g,b, self.NUM)
+
+        new_r_matrix = {}
+        new_g_matrix = {}
+        new_b_matrix = {}
+
+        for k in r_matrix:
+            new_r_matrix[k],new_g_matrix[k],new_b_matrix[k] = \
+                self.divide_in_blocks(r_matrix[k], g_matrix[k], b_matrix[k], 2)
+
+        p_r, v_r, p_g, v_g, p_b, v_b = {}, {}, {}, {}, {}, {}
+
+        for a in new_r_matrix:
+            for b in new_r_matrix[a]:
+                v_r[(a, b)] = self.get_bit_value(new_r_matrix[a][b][(0, 0)], 1)
+                p_r[(a, b)] = self.get_bit_value(new_r_matrix[a][b][(0, 1)], 1)
+                v_g[(a, b)] = self.get_bit_value(new_g_matrix[a][b][(0, 0)], 1)
+                p_g[(a, b)] = self.get_bit_value(new_g_matrix[a][b][(0, 1)], 1)
+                v_b[(a, b)] = self.get_bit_value(new_b_matrix[a][b][(0, 0)], 1)
+                p_b[(a, b)] = self.get_bit_value(new_b_matrix[a][b][(0, 1)], 1)
+
+        # Set the two LSB's of each pixel within B's to zero
+        for k in new_r_matrix:
+            for j in new_r_matrix[k]:
+                for x in range(len(new_r_matrix[k][j])):
+                    for y in range(len(new_r_matrix[k][j][0])):
+                        new_r_matrix[k][j][(x,y)] = self.change_lsb(new_r_matrix[k][j][(x,y)], 2, 0)
+                        new_g_matrix[k][j][(x,y)] = self.change_lsb(new_g_matrix[k][j][(x,y)], 2, 0)
+                        new_b_matrix[k][j][(x,y)] = self.change_lsb(new_b_matrix[k][j][(x,y)], 2, 0)
+
+        # And compute the avg intensity
+
+        alpha_r, alpha_g, alpha_b = {}
+
+        for a in new_r_matrix:
+            for b in new_r_matrix[a]:
+                alpha_r[(a,b)] = self.avg_int(new_r_matrix[a][b])
+                alpha_g[(a,b)] = self.avg_int(new_g_matrix[a][b])
+                alpha_b[(a,b)] = self.avg_int(new_b_matrix[a][b])
+
+        # Count the total number of 1s in avg_B and denote it N's
+
+        cor = {}
+
+        for v in r_matrix:
+            cor[v] = ((k*v)%self.N)
+
+        i = 0
+        Ak = i
+        Bk = cor[i]
+        while True:
+
+            new_r_matrix[Bk]["avg"] = {"avg":self.avg_int(r_matrix[Bk])}
+            new_g_matrix[Bk]["avg"] = {"avg":self.avg_int(g_matrix[Bk])}
+            new_b_matrix[Bk]["avg"] = {"avg":self.avg_int(b_matrix[Bk])}
+            new_r_matrix[Ak]["avg"] = {"avg":self.avg_int(r_matrix[Ak])}
+            new_g_matrix[Ak]["avg"] = {"avg":self.avg_int(g_matrix[Ak])}
+            new_b_matrix[Ak]["avg"] = {"avg":self.avg_int(b_matrix[Ak])}
+            new_r_matrix[Bk]["3-tuple"] = {}
+            new_g_matrix[Bk]["3-tuple"] = {}
+            new_b_matrix[Bk]["3-tuple"] = {}
+            for j in new_r_matrix[Bk]:
+                if j != "avg" and j != "3-tuple":
+                    new_r_matrix[Ak]["avg"][j] = self.avg_int(new_r_matrix[Ak][j])
+                    new_g_matrix[Ak]["avg"][j] = self.avg_int(new_g_matrix[Ak][j])
+                    new_b_matrix[Ak]["avg"][j] = self.avg_int(new_b_matrix[Ak][j])
+
+                    r_truncated_A = self.truncate_x_lsb(new_r_matrix[Ak]["avg"][j], 2)
+                    g_truncated_A = self.truncate_x_lsb(new_g_matrix[Ak]["avg"][j], 2)
+                    b_truncated_A = self.truncate_x_lsb(new_b_matrix[Ak]["avg"][j], 2)
+
+                    new_r_matrix[Bk]["avg"][j] = self.avg_int(new_r_matrix[Bk][j])
+                    new_r_matrix[Bk]["3-tuple"][j] = {}
+                    new_r_matrix[Bk]["3-tuple"][j]["v"] = \
+                        1 if new_r_matrix[Bk]["avg"][j] >= new_r_matrix[Bk]["avg"]["avg"] else 0
+                    if (self.count_bit_msb(new_r_matrix[Bk]["avg"][j], 6, 1) % 2 ) != p_r or \
+                            new_r_matrix[Bk]["3-tuple"][j]["v"]  != v_r[(Bk,j)]:
+                        erroneous = True
+                    new_g_matrix[Bk]["avg"][j] = self.avg_int(new_g_matrix[Bk][j])
+                    new_g_matrix[Bk]["3-tuple"][j] = {}
+                    new_g_matrix[Bk]["3-tuple"][j]["v"] = \
+                        1 if new_g_matrix[Bk]["avg"][j] >= new_g_matrix[Bk]["avg"]["avg"] else 0
+                    if (self.count_bit_msb(new_g_matrix[Bk]["avg"][j], 6, 1) % 2) != p_g or \
+                            new_g_matrix[Bk]["3-tuple"][j]["v"]  != v_g[(Bk,j)]:
+                        erroneous = True
+                    new_b_matrix[Bk]["avg"][j] = self.avg_int(new_b_matrix[Bk][j])
+                    new_b_matrix[Bk]["3-tuple"][j] = {}
+                    new_b_matrix[Bk]["3-tuple"][j]["v"] = \
+                        1 if new_b_matrix[Bk]["avg"][j] >= new_b_matrix[Bk]["avg"]["avg"] else 0
+                    if (self.count_bit_msb(new_b_matrix[Bk]["avg"][j], 6, 1) % 2) != p_b or \
+                            new_b_matrix[Bk]["3-tuple"][j]["v"]  != v_b[(Bk,j)]:
+                        erroneous = True
+
+
+            if Bk == i:
+                break
+            Ak = Bk
+            Bk = cor[Ak]
+
+            print(Ak, Bk)
+
+        return erroneous, new_r_matrix, new_b_matrix, new_g_matrix
