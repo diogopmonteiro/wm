a253dfa0c287758a85c636c6bf8b302877e2192d
Pedro Reganha <pedro.reganha@gmail.com>
2015-12-04 04:58:55 +0000

    watermark is being inserted. good.

--- core/algs/recover.py
+++ core/algs/recover.py
@@ -1,6 +1,8 @@
 from abstract import Algorithm
 from PIL import Image
 import random
+from bitstring import BitArray
+import numpy as np
 
 '''
     http://www1.pu.edu.tw/~lan/papers/watermarking.pdf
@@ -28,19 +30,162 @@ class Recover(Algorithm):
         cor = {}
         
         for v in r_matrix:
-            cor[v] = ((k*v)%self.N)+1
+            cor[v] = ((k*v)%self.N)
+
+        # Block watermark embedding
+
+        new_r_matrix = {}
+        new_g_matrix = {}
+        new_b_matrix = {}
+
+
+        for k in r_matrix:
+            new_r_matrix[k],new_g_matrix[k],new_b_matrix[k] = \
+                self.divide_in_blocks(r_matrix[k], g_matrix[k], b_matrix[k], 2)
+
+
+        # Sub-block watermark generation and embedding algorithm
+        # Set the two LSBs of each pixel within the block to zero
+
+        for k in new_r_matrix:
+            for j in new_r_matrix[k]:
+                for x in range(len(new_r_matrix[k][j])):
+                    for y in range(len(new_r_matrix[k][j][0])):
+                        new_r_matrix[k][j][(x,y)] = self.change_lsb(new_r_matrix[k][j][(x,y)], 2, 0)
+                        new_g_matrix[k][j][(x,y)] = self.change_lsb(new_g_matrix[k][j][(x,y)], 2, 0)
+                        new_b_matrix[k][j][(x,y)] = self.change_lsb(new_b_matrix[k][j][(x,y)], 2, 0)
+
+        # Compute the average intensity of the block and each
+        # of its four sub-blocks, denoted by avg_{r,g,b}(calculated before) and avg_{r,g,b}' ,respectively.
+        # Generate the authentication watermark v of each sub-block
+        # Generate the parity-check bit p of each sub-block
+
+        # From the mapping sequence generated in the preparation step, obtain block A whose recovery
+        # information will be storedin block B. {cor}
+
+        # Compute the average intensity of each corresponding sub-block A
+
+        # point 6, 7
+
+
+        i = 0
+        Ak = i
+        Bk = cor[i]
+        while True:
+
+            new_r_matrix[Bk]["avg"] = {"avg":self.avg_int(r_matrix[Bk])}
+            new_g_matrix[Bk]["avg"] = {"avg":self.avg_int(g_matrix[Bk])}
+            new_b_matrix[Bk]["avg"] = {"avg":self.avg_int(b_matrix[Bk])}
+            new_r_matrix[Ak]["avg"] = {"avg":self.avg_int(r_matrix[Ak])}
+            new_g_matrix[Ak]["avg"] = {"avg":self.avg_int(g_matrix[Ak])}
+            new_b_matrix[Ak]["avg"] = {"avg":self.avg_int(b_matrix[Ak])}
+            new_r_matrix[Bk]["3-tuple"] = {}
+            new_g_matrix[Bk]["3-tuple"] = {}
+            new_b_matrix[Bk]["3-tuple"] = {}
+            for j in new_r_matrix[Bk]:
+                if j != "avg" and j != "3-tuple":
+                    new_r_matrix[Ak]["avg"][j] = self.avg_int(new_r_matrix[Ak][j])
+                    new_g_matrix[Ak]["avg"][j] = self.avg_int(new_g_matrix[Ak][j])
+                    new_b_matrix[Ak]["avg"][j] = self.avg_int(new_b_matrix[Ak][j])
+
+                    r_truncated_A = self.truncate_x_lsb(new_r_matrix[Ak]["avg"][j], 2)
+                    g_truncated_A = self.truncate_x_lsb(new_g_matrix[Ak]["avg"][j], 2)
+                    b_truncated_A = self.truncate_x_lsb(new_b_matrix[Ak]["avg"][j], 2)
+
+                    new_r_matrix[Bk]["avg"][j] = self.avg_int(new_r_matrix[Bk][j])
+                    new_r_matrix[Bk]["3-tuple"][j] = {}
+                    new_r_matrix[Bk]["3-tuple"][j]["v"] = \
+                        1 if new_r_matrix[Bk]["avg"][j] >= new_r_matrix[Bk]["avg"]["avg"] else 0
+                    new_r_matrix[Bk]["3-tuple"][j]["p"] = self.count_bit_msb(new_r_matrix[Bk]["avg"][j], 6, 1) % 2
+                    self.embed_matrix_lsb(
+                        new_r_matrix[Bk][j],
+                        new_r_matrix[Bk]["3-tuple"][j]["v"],
+                        new_r_matrix[Bk]["3-tuple"][j]["p"],
+                        r_truncated_A
+                    )
+                    new_g_matrix[Bk]["avg"][j] = self.avg_int(new_g_matrix[Bk][j])
+                    new_g_matrix[Bk]["3-tuple"][j] = {}
+                    new_g_matrix[Bk]["3-tuple"][j]["v"] = \
+                        1 if new_g_matrix[Bk]["avg"][j] >= new_g_matrix[Bk]["avg"]["avg"] else 0
+                    new_g_matrix[Bk]["3-tuple"][j]["p"] = self.count_bit_msb(new_g_matrix[Bk]["avg"][j], 6, 1) % 2
+                    self.embed_matrix_lsb(
+                        new_g_matrix[Bk][j],
+                        new_g_matrix[Bk]["3-tuple"][j]["v"],
+                        new_g_matrix[Bk]["3-tuple"][j]["p"],
+                        g_truncated_A
+                    )
+                    new_b_matrix[Bk]["avg"][j] = self.avg_int(new_b_matrix[Bk][j])
+                    new_b_matrix[Bk]["3-tuple"][j] = {}
+                    new_b_matrix[Bk]["3-tuple"][j]["v"] = \
+                        1 if new_b_matrix[Bk]["avg"][j] >= new_b_matrix[Bk]["avg"]["avg"] else 0
+                    new_b_matrix[Bk]["3-tuple"][j]["p"] = self.count_bit_msb(new_b_matrix[Bk]["avg"][j], 6, 1) % 2
+                    self.embed_matrix_lsb(
+                        new_b_matrix[Bk][j],
+                        new_b_matrix[Bk]["3-tuple"][j]["v"],
+                        new_b_matrix[Bk]["3-tuple"][j]["p"],
+                        b_truncated_A
+                    )
+            if Bk == i:
+                break
+            Ak = Bk
+            Bk = cor[Ak]
+
+            print(Ak, Bk)
 
         return image
 
-    def bin(self, s):
-       return str(s) if s<=1 else bin(s>>1) + str(s&1)
+    def embed_matrix_lsb(self, matrix, v, p, r):
+        vpr = str(v) + str(p) + str(r)
+        for x in range(len(matrix)):
+            for y in range(len(matrix[0])):
+                self.embed_pixel_lsb(matrix, (x,y) ,vpr[x]+vpr[x+4])
+
+
+    def embed_pixel_lsb(self, matrix, i, to_emb):
+        snum = "{0:b}".format(matrix[i])
+        if len(snum) <= len(to_emb):
+            b = BitArray(bin=to_emb)
+            matrix[i] = b.uint
+            return
+        snum = snum[:len(to_emb)*-1] + to_emb
+        b = BitArray(bin=snum)
+        matrix[i] = b.uint
+
+
+    def truncate_x_lsb(self, number, x):
+        snum = "{0:b}".format(number)
+        if len(snum) <= x:
+            return "0"*(8-x)
+
+        return snum[:-1*x] if len(snum[:-1*x]) == (8-x) else "0"*(8-len(snum[:-1*x]))+snum[:-1*x]
+
+    def count_bit_msb(self, number, num_of_bits, n_val):
+        snum = "{0:b}".format(number)
+        if len(snum) < num_of_bits:
+            snum = (num_of_bits - len(snum))*"0"+snum
+        count = 0
+        for i in range(0, num_of_bits):
+            count += 1 if snum[i] == str(n_val) else 0
+        return count
+
+    def avg_int(self, matrix):
+        return int(np.matrix(matrix).mean())
+
+    def change_lsb(self, number, num_of_bits, n_val):
+        snum = "{0:b}".format(number)
+        if len(snum) <= num_of_bits:
+            b = BitArray(bin=str(n_val)*num_of_bits)
+            return b.uint
+        snum = snum[:(num_of_bits)*-1] + str(n_val)*num_of_bits
+        b = BitArray(bin=snum)
+        return b.uint
 
 
     def is_prime(self, a):
         return all(a % i for i in xrange(2, a))
 
     def get_k(self,n):
-        primes = [i for i in range(0,n) if self.is_prime(i)]
+        primes = [i for i in range(1,n) if self.is_prime(i)]
         n = random.choice(primes)
         return n
 
@@ -50,12 +195,18 @@ class Recover(Algorithm):
         b_matrix = {}
         k = num
         z = num
-        self.N = (len(r)/self.NUM)**2
+        x = 0
+        y = 0
+        self.N = (len(r)/num)**2
+        num = len(r)/num
         for i in range(k, len(r)+1, k):
             for j in range(z, len(r[0])+1, z):
-                r_matrix[j+(i*self.N)] = r[i-k:i, j-z:j]
-                g_matrix[j+(i*self.N)] = g[i-k:i, j-z:j]
-                b_matrix[j+(i*self.N)] = b[i-k:i, j-z:j]
+                r_matrix[y+(x*num)] = r[i-k:i, j-z:j]
+                g_matrix[y+(x*num)] = g[i-k:i, j-z:j]
+                b_matrix[y+(x*num)] = b[i-k:i, j-z:j]
+                y += 1
+            x += 1
+            y = 0
         return r_matrix, g_matrix, b_matrix
 
     def extract_specific(self, image, watermark):
